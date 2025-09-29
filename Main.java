a5b1c5820a2bf6e0cfcd28afb6eaeab71e90da51
imtoatb <idfc.gfy@gmail.com>
2025-09-29 12:35:15 +0530

    Initialization of the user file with the register and log in and of the main file with the menu

--- Main.java
+++ Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        int id = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("************** Welcome on MAHE Music **************");
+        System.out.println("1. Register");
+        System.out.println("2. Log In");
+        System.out.println("***************************************************");
+
+        int choice = sc.nextInt();
+
+        switch(choice){
+
+            case 1:
+                System.out.println("Create your account !");
+                System.out.println("Enter your name:");
+                String name = sc.nextLine();
+
+                System.out.println("Who are you ? (Admin, User, Premium User)");
+                String status = sc.nextLine();
+
+                
+
+                if (status == "Admin"){
+                    Admin name = new Admin(name, id);
+                }
+                else if (status == "User"){
+                    User name = new User(name, id);
+                }
+                else if (status == "Premium User"){
+                    PremiumUser name = new PremiumUser(name, id);
+                }
+                else{
+                    System.out.println("Something went wrong");
+                }
+
+                System.out.println("Welcome " + name + ", here's your id : " + id);
+                id++;
+        }
+
+
     }
 }
--- User.java
+++ User.java
@@ -1,3 +1,42 @@
+
 public class User {
+    String name;
+    String userId;
+    boolean connected;
+    
+    public User(String name, String useId){
+        this.name = name;
+        this.userId = useId;
+        this.connected = false;
+    }
+
+    public String register(String name, String userId){
+        this.name = name;
+        this.userId = userId;
+        return "Your account has been created";
+    }
+
+    public String logIn(){
+        if (connected == true){
+            return "You're already connected";
+        }    
+        else{
+            connected = true;
+            return "You're now on your account";
+        }
+    }
+
+    public String logOut(){
+        if (connected === false){
+            return "A problem has occur, please reach the admin for more informations";
+        }
+        else{
+        connected = false;
+        return "You've been disconnected";
+        }
+
+    }
+
+
     
 }
